94c7d5a42bdccdce0652dd3b812e2b2adb216a7e
Robin <robin.james.guerrier@gmail.com>
2022-12-04 16:56:08 -0500

    Use api data to generate the question components and the answer inputs within them.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Question from './components/Question';
+import { nanoid } from 'nanoid';
 
 export default function App() {
 
     const [ triviaRoundData, setTriviaRoundData ] = useState([]);
-    const [ playerAnswers, setPlayerAnswers ] = useState({});
+    // const [ playerAnswers, setPlayerAnswers ] = useState({});
 
     useEffect(() => {
         fetch('https://opentdb.com/api.php?amount=5')
@@ -12,10 +13,19 @@ export default function App() {
             .then((data) => setTriviaRoundData(data.results));
     }, []);
 
+    const questions = triviaRoundData.map((item) => (
+        <Question 
+            key={nanoid()}
+            question={item.question}
+            correctAnswer={item.correct_answer}
+            incorrectAnswers={item.incorrect_answers}
+        />
+    ));
+
     return (
         <main>
             <form>
-                <Question />
+                {questions}
                 <button>Check Answers</button>
             </form>
         </main>
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import he from 'he';
+import { nanoid } from 'nanoid';
 
-export default function Question() {
+export default function Question(props) {
+
+    const { question, correctAnswer, incorrectAnswers } = props;
+
+    const answersArray = (() => {
+        const newArray = [...incorrectAnswers];
+        const randomIndexNumber = Math.floor(Math.random() * newArray.length);
+        newArray.splice(randomIndexNumber, 0, correctAnswer);
+        return newArray;
+    })();
+
+    const answerElements = answersArray.map((answer) => (
+        <div key={nanoid()}>
+            <input type='radio' id='test' />
+            <label htmlFor='test'>{answer}</label>
+        </div>
+    ));
+
     return (
         <div>
-            <p>Test test test test test test test test.</p>
-            <input type='radio' id='test' />
-            <label htmlFor='test'>TEST</label>
+            <p>{he.decode(question)}</p>
+            {answerElements}
         </div>
     );
 };
