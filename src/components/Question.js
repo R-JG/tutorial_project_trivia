1fe71d4014a3e94b159e5dc0bb1ea9096497714b
Robin <robin.james.guerrier@gmail.com>
2022-12-03 21:25:24 -0500

    Setup app structure with Question component for form, and set trivia round state from api call.

---
+++ src/components/Question.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Question() {
+    return (
+        <div>
+            <p>Test test test test test test test test.</p>
+            <input type='radio' id='test' />
+            <label htmlFor='test'>TEST</label>
+        </div>
+    );
+};
